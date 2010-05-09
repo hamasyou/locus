f46f30b03b53f688df64a295dd11009899c41c28
Syougo.Hamada <hamasyou@gmail.com>
2010-05-09 21:13:25 +0900

    ScaffoldによるCRUDフォームを作成しました。
      - ruby script/generate scaffold を使ってScaffoldを作成しました
      - Scaffoldのレイアウトをブログのスタイルに変更しました
      - HTMLコードをDRYにしました

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20100509111527) do
+
+  create_table "entries", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
