e76418e04a28fa18f8fcea68797dd66fd97114c3
Syougo.Hamada <hamasyou@gmail.com>
2010-05-10 00:28:28 +0900

    画像をアップロードできるようにしました。
      - file_column プラグインを追加しました
      - file_column 619行目の Inflector のバグを修正しました
      - 画像が表示されるように _form.html.erb を修正しました

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,4 @@
 class Entry < ActiveRecord::Base
+  file_column :image, :magick => { :geometry => "458x360>" }
   validates_presence_of :title, :content
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100509143314) do
+ActiveRecord::Schema.define(:version => 20100509150259) do
 
   create_table "entries", :force => true do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "users", :force => true do |t|
