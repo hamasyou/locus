3288bfa61bd4768e2e3973411c346258e7ed987a
Syougo.Hamada <hamasyou@gmail.com>
2010-05-09 22:13:21 +0900

    国際化対応を行いました。
      - gettextパッケージをインストールしました（gem で入れたので関係ないか。。）
      - environment.rb、application_controllerにgettextを使うための設定を記述しました
      - モデル entry に必須チェックを追加しました

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  
+  config.gem "locale_rails"
+  config.gem "gettext_activerecord"
+  config.gem "gettext_rails"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
